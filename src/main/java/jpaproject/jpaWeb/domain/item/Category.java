0577c07373adc24f52d67eff5e5100db169e6053
권오성 <kos9310@gmail.com>
2021-12-17 17:12:57 +0900

    도메인 작성

---
+++ src/main/java/jpaproject/jpaWeb/domain/item/Category.java
@@ -0,0 +1,26 @@
+package jpaproject.jpaWeb.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+
+    @Id @GeneratedValue
+    @Column(name = "catetory_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "cateory_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns =  @JoinColumn(name = "item_id"))
+    private List<Item> items = new ArrayList<Item>();
+
+}
