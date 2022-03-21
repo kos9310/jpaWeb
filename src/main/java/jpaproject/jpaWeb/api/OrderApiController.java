50435a98ed92260cd9bd118f613e19a8e4ec0bd1
권오성 <kos9310@gmail.com>
2022-03-21 22:03:12 +0900

    주문 조회 V1: 엔티티 직접 노출

---
+++ src/main/java/jpaproject/jpaWeb/api/OrderApiController.java
@@ -0,0 +1,30 @@
+package jpaproject.jpaWeb.api;
+
+import jpaproject.jpaWeb.domain.Order;
+import jpaproject.jpaWeb.domain.OrderItem;
+import jpaproject.jpaWeb.repository.OrderRepository;
+import jpaproject.jpaWeb.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+    
+    private final OrderRepository orderRepository;
+    
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+}
