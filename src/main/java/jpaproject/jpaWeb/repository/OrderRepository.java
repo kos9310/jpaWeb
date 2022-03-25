00822b952caf6244f277f8340708c4c901446c9c
권오성 <kos9310@gmail.com>
2022-03-25 23:41:30 +0900

    주문 조회 V3.1: 엔티티를 DTO로 변환 - 페이징과 한계 돌파

--- src/main/java/jpaproject/jpaWeb/repository/OrderRepository.java
+++ src/main/java/jpaproject/jpaWeb/repository/OrderRepository.java
@@ -94,7 +94,6 @@ public class OrderRepository {
         ).getResultList();
     }
 
-
     public List<Order> findAllWithItem() {
         return em.createQuery(
                 "select distinct o from Order o" +
@@ -102,6 +101,16 @@ public class OrderRepository {
                 " join fetch o.delivery d" +
                 " join fetch o.orderItems oi" +
                 " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class
+                ).setFirstResult(offset)
+                .setMaxResults(limit)
                 .getResultList();
     }
 }
